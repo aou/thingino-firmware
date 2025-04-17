051f9b1f141fc4fa442cdb8ca5ed8db40aaba706
Alexander Ou <alexou@gmail.com>
2025-04-17 15:48:07 -0700

    flake init

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Example flake environment for build buildroot projects";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShells.${system}.default = (pkgs.buildFHSUserEnv {
+      name = "buildroot";
+      targetPkgs = pkgs: (with pkgs;
+        [
+          (lib.hiPrio gcc)
+	  bash
+	  dialog
+          file
+          gnumake
+          ncurses.dev
+	  newt
+          pkg-config
+          unzip
+          wget
+          pkgsCross.aarch64-multiplatform.gccStdenv.cc
+        ] ++ pkgs.linux.nativeBuildInputs);
+    }).env;
+  };
+}
